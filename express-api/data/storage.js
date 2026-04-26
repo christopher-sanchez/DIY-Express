d19f5123d86a4c92f9492ca5304988fbef11d2ee
christopher-sanchez <csanchez@my.okcu.edu>
2026-04-26 15:14:20 -0500

    storage

---
+++ express-api/data/storage.js
@@ -0,0 +1,23 @@
+export const tasks = [
+    {id:1, title:'Learn Node.js', completed:
+        false, categoryId:1
+    },
+    {id: 2, title: 'Build HTTP Server', completed:
+        true, categoryId:1
+    },
+    {id:3, title:'Learn Express', completed:
+        false, categoryId:1
+    }
+];
+
+export const categories = [
+    {id:1, name: 'Learning'},
+    {id:2, name: 'Work'},
+    {id:3, name: 'Personal'}
+];
+
+export const getNextId = (array) =>{
+    if(array.length === 0) return 1;
+    return Math.max(...array.map(item => item.id))+
+    1;
+};
